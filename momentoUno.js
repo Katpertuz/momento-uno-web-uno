7e36ca1238adb274b8a4927fb07df2701e5c3ce1
Katpertuz <katherin.o.pertuz@gmail.com>
2026-09-21 15:04:32 -0500

    feat: se inicia proyecto momento uno con estructura general del html, se crear archivo .js, se relacionan y se colocan primeras varibles

---
+++ momentoUno.js
@@ -0,0 +1,6 @@
+const NOMBRE_USUARIO= admin;
+const CONTRASENIA= 1230;
+let intentos=3;
+let condition=true;
+let usuario=prompt(`Ingrese su usuario `);
+let contrasenia=prompt(`Ingrese su contraseña `);
