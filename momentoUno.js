38ccacae0866611a88cff2010706d5c34a15af1f
KatPertuz <katherin.o.pertuz@gmail.com>
2026-09-22 07:50:47 -0500

    Feat: Se agregan las dos funciones de validacion de usuario y de contraseña

--- momentoUno.js
+++ momentoUno.js
@@ -4,3 +4,12 @@ let intentos=3;
 let condition=true;
 let usuario=prompt(`Ingrese su usuario `);
 let contrasenia=prompt(`Ingrese su contraseña `);
+
+
+function  usuarioValido(usuario, NOMBRE_USUARIO){
+  return (usuario===NOMBRE_USUARIO);
+} 
+
+function contraseniaValida(CONTRASENIA, contrasenia){
+    return (contrasenia===CONTRASENIA);
+}
