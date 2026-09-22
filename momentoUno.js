4caecb1f0a191306f053b10b876c351cd9f698fa
KatPertuz <katherin.o.pertuz@gmail.com>
2026-09-22 08:12:36 -0500

    Feat: Se llaman afuera las funciones, se inicia el ciclo while, con la condicion de que entra el usuario y contraseña correctas y el primer intento fallido de ingresar la contraseña. Fix: se ingresan las variables usuario y contrasenia al ciclo ya que solo se necesitan dentro de este

--- momentoUno.js
+++ momentoUno.js
@@ -1,15 +1,32 @@
-const NOMBRE_USUARIO= admin;
-const CONTRASENIA= 1230;
-let intentos=3;
-let condition=true;
-let usuario=prompt(`Ingrese su usuario `);
-let contrasenia=prompt(`Ingrese su contraseña `);
+const NOMBRE_USUARIO = admin;
+const CONTRASENIA = 1230;
+let intentos = 3;
+let condition = true;
 
 
-function  usuarioValido(usuario, NOMBRE_USUARIO){
-  return (usuario===NOMBRE_USUARIO);
-} 
+while (intentos > 0) {
 
-function contraseniaValida(CONTRASENIA, contrasenia){
-    return (contrasenia===CONTRASENIA);
+    let usuario = prompt(`Ingrese su usuario `);
+    let contrasenia = prompt(`Ingrese su contraseña `);
+
+    if (usuarioValido && contraseniaValida) {
+        console.log(`Bienvenido al sistema `);
+        condition = false
+    } else {
+        prompt(`Datos incorrectos, intento  ${intentos} de  ${intentos + 1} `)
+        intentos--;
+    }
+
 }
+console.log(`Salimos del ciclo`);
+
+function usuarioValido(usuario, NOMBRE_USUARIO) {
+    return (usuario === NOMBRE_USUARIO);
+}
+
+function contraseniaValida(CONTRASENIA, contrasenia) {
+    return (contrasenia === CONTRASENIA);
+}
+
+let vallidadcionUsuario = usuarioValido(NOMBRE_USUARIO, usuario);
+let validacionContrasenia = contraseniaValida(contrasenia, CONTRASENIA);
