ac64b187cd3c5e98fd5cf9bc39988fb2460ceb81
KatPertuz <katherin.o.pertuz@gmail.com>
2026-09-24 06:02:24 -0500

    fix: se ajusta el condicional else para que no muestre cuando ya no tiene deseos, si no que solo pregunte por lo intentos hasta que le quede un deseo, cuando los acaba pasa al siguiente if en donde lo bloquea y sale del sistema

--- momentoUno.js
+++ momentoUno.js
@@ -11,22 +11,23 @@ while (condition) {
     if (usuarioValido(usuario, NOMBRE_USUARIO) && contraseniaValida(contrasenia, CONTRASENIA)) {
         console.log(`Bienvenido al sistema `);
         condition = false
-    } else {intentos--
-        console.log(`Datos incorrectos, intento  ${intentos} de 3 ingrese nuevamente sus datos `);
-        ;
+    } else {
+        intentos--
+
+        if (intentos > 0) {
+            console.log(`Datos incorrectos, intento  ${intentos} de 3 ingrese nuevamente sus datos `);
+        }}
+        if (intentos === 0) {
+            console.log(`Usuario bloqueado, has superado el número de intentos`);
+            condition = false;
+        }
+
     }
-    if (intentos === 0) {
-        console.log(`Usuario bloqueado, has superado el número de intentos`);
-        condition = false;
+
+    function usuarioValido(usuario, NOMBRE_USUARIO) {
+        return (usuario === NOMBRE_USUARIO);
     }
 
-}
-
-
-function usuarioValido(usuario, NOMBRE_USUARIO) {
-    return (usuario === NOMBRE_USUARIO);
-}
-
-function contraseniaValida(CONTRASENIA, contrasenia) {
-    return (contrasenia === CONTRASENIA)
-} 
+    function contraseniaValida(CONTRASENIA, contrasenia) {
+        return (contrasenia === CONTRASENIA)
+    } 
